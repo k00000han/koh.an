555871bd3bba4f723ed3940c5a34b135b180b86f
kohan <kohannnnn@gmail.com>
2025-01-15 21:10:46 +0100

    Fix code style

--- src/components/navbar/NavbarAnimation.tsx
+++ src/components/navbar/NavbarAnimation.tsx
@@ -11,15 +11,15 @@ const DropdownAnimation: FC<DropdownAnimationProps> = ({isMenuOpen, children}) =
     <AnimatePresence>
       {isMenuOpen && (
         <motion.div
-          className="dropdown-conteiner"
-            initial={{x: 0, y: -400, opacity: 20, filter: 'blur(0px)'}}
-            animate={{x: 0, y: 0, opacity: 10, filter: 'blur(0px)'}}
-            exit={{x: 0, y: -400, opacity: 20, filter: 'blur(0px)'}}
-            transition={{
-              duration: 0.7,
-              ease: [0.8, 0, 0.58, 1],
-              delay: 0.2
-            }}
+        className="dropdown-conteiner"
+        initial={{x: 0, y: -400, opacity: 20, filter: 'blur(0px)'}}
+        animate={{x: 0, y: 0, opacity: 10, filter: 'blur(0px)'}}
+        exit={{x: 0, y: -400, opacity: 20, filter: 'blur(0px)'}}
+        transition={{
+        duration: 0.7,
+        ease: [0.8, 0, 0.58, 1],
+        delay: 0.2
+        }}
         >
           {children}
         </motion.div>
