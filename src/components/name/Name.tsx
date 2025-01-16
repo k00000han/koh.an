50539550a9c2ef7b483523dc94c45f236b3a6890
kohan <kohannnnn@gmail.com>
2025-01-17 00:20:57 +0100

    Implementing reveal animation
    - fixed wrong behaviour with cursor

--- src/components/name/Name.tsx
+++ src/components/name/Name.tsx
@@ -6,13 +6,15 @@ const Name: FC = () => {
   return (
     <div className="name-conteiner">
       <div className="name-introduction">
-        <div className="name-element">
-          Kohan<br />
-          Constantin
-        </div>
-        <div className="specialisation-conteiner">
-          <p className="specialisation-title">Specialisation:</p>
-          <div className="specialisation-content">Full-Stack Software Engineer</div>
+        <div className="name-animation-conteiner">
+          <div className="name-element">
+            Kohan<br />
+            Constantin
+          </div>
+          <div className="specialisation-conteiner">
+            <p className="specialisation-title">Specialisation:</p>
+            <div className="specialisation-content">Full-Stack Software Engineer</div>
+          </div>
         </div>
       </div>
       <div className="name-gradient-1"></div>
