03ebd883468d0dd307cf100874bcd14328c05feb
kohan <kohannnnn@gmail.com>
2025-02-01 20:10:07 +0100

    Fix CV

--- src/components/text-block/TextBlock.tsx
+++ src/components/text-block/TextBlock.tsx
@@ -8,7 +8,7 @@ const TextBlock = () => {
       buttonText="Check CV"
       btnClassName="text-button-two"
       btnTextClassName="text-button-text-two"
-      cvPath="src/assets/pdf/noCV.pdf"
+      cvPath="public/noCV.pdf"
     />
   );
 }
