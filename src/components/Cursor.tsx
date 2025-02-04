130a02f80bdccbd669e4453504bd45553dfcb769
kohan <kohannnnn@gmail.com>
2025-02-04 18:46:38 +0100

    Fix Cursor animation
    - replacing vanilla implementation on GSAP lib, because animation had some glitches on Safari

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,15 +1,23 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 
 import '../styles/components/Cursor.sass';
 
 const Cursor = () => {
-  const cursorRef = useRef<HTMLDivElement>(null);
+  const cursorRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleMouseMove = (e: { clientX: any; clientY: any; }): void => {
-      if (cursorRef.current) {
-        cursorRef.current.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
-      }
+    const handleMouseMove = (e: MouseEvent): void => {
+      const cursor: HTMLDivElement | null = cursorRef.current;
+      const cursorWidth: number = cursor?.offsetWidth || 0;
+      const cursorHeight: number = cursor?.offsetHeight || 0;
+
+      gsap.to(cursor, {
+        x: e.clientX - cursorWidth / 2,
+        y: e.clientY - cursorHeight / 2,
+        duration: 0.2,
+        ease: "power2.out",
+      });
     };
 
     document.addEventListener('mousemove', handleMouseMove);
